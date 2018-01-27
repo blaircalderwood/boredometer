2051a13de9ef23e96131dd7f82df7d11c6eceacc
Blair Calderwood <blaircalderwood@live.co.uk>
2018-01-27 15:52:58 +0000

    Bug/clean up project (#5)
    * Refactored code and integrated lesson methods into model
    
    * Fixed lesson rerouting

--- boredometer/urls.py
+++ boredometer/urls.py
@@ -4,11 +4,12 @@ from boredometer_app import views as main_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('vote/', main_views.main_screen, name='vote'),
+    path('', main_views.main_screen, name='main_screen'),
+    path('vote/', main_views.main_screen),
     path('vote/<str:lesson_id>/', main_views.voting_screen, name='vote'),
     path('vote/<str:lesson_id>/bored', main_views.bored, name='bored'),
-    path('', main_views.main_screen, name='main_screen'),
     path('create/', main_views.create, name='create'),
+    path('lesson/', main_views.main_screen),
     path('lesson/<str:lesson_id>/', main_views.view_lesson, name='view_lesson'),
     path('lesson/<str:lesson_id>/update', main_views.update_teachers_lesson, name='update_lesson'),
     path('lesson/<str:lesson_id>/clearbored', main_views.clear_bored, name='clear_bored'),
