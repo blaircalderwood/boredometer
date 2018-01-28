4726d8e02a5263c1a275a532d793ea2e3bfcfce5
Blair Calderwood <blaircalderwood@live.co.uk>
2018-01-28 12:52:56 +0000

    Feature/create participants (#16)
    * Created add participants endpoint to hit on AJAX call
    
    * Updated git ignore file
    
    * Added pip requirements file
    
    * Fixed add participants bug

--- boredometer_app/models.py
+++ boredometer_app/models.py
@@ -4,64 +4,63 @@ from uuid import uuid4
 
 
 class Lesson(models.Model):
-    id = models.CharField(unique=True, max_length=6, primary_key=True)
-    section_number = models.IntegerField(default=0)
-    participants = models.IntegerField(default=0)
-    amount_bored = models.IntegerField(default=0)
+	id = models.CharField(unique=True, max_length=6, primary_key=True)
+	section_number = models.IntegerField(default=0)
+	participants = models.IntegerField(default=0)
+	amount_bored = models.IntegerField(default=0)
 
-    def __str__(self):
-        return str(self.id)
+	def __str__(self):
+		return str(self.id)
 
-    @staticmethod
-    def get(lesson_id):
-        lesson_id = lesson_id.lower()
-        try:
-            return Lesson.objects.get(id=lesson_id)
-        except ObjectDoesNotExist:
-            raise ObjectDoesNotExist
+	@staticmethod
+	def get(lesson_id):
+		lesson_id = lesson_id.lower()
+		try:
+			return Lesson.objects.get(id=lesson_id)
+		except ObjectDoesNotExist:
+			raise ObjectDoesNotExist
 
-    def add_participant(self, participant_id=''):
-        print(participant_id)
-        try:
-            if participant_id is not '':
-                participant = Participant.objects.get(id=participant_id, lesson=self)
-            else:
-                participant = self.increase_participants()
-        except ObjectDoesNotExist:
-            participant = self.increase_participants()
+	def add_participant(self, participant_id=''):
+		try:
+			if participant_id is not '':
+				participant = Participant.objects.get(id=participant_id, lesson=self)
+			else:
+				participant = self.increase_participants()
+		except ObjectDoesNotExist:
+			participant = self.increase_participants(participant_id)
 
-        return participant.id
+		return participant.id
 
-    def increase_participants(self):
-        participant = Participant.objects.create(lesson=self)
-        self.participants += 1
-        self.save()
-        return participant
+	def increase_participants(self, participant_id=''):
+		participant = Participant.objects.create(lesson=self, id=participant_id)
+		self.participants += 1
+		self.save()
+		return participant
 
-    def add_to_bored(self):
-        self.amount_bored += 1
-        self.save()
+	def add_to_bored(self):
+		self.amount_bored += 1
+		self.save()
 
-    def clear_bored(self):
-        self.amount_bored = 0
-        self.save()
+	def clear_bored(self):
+		self.amount_bored = 0
+		self.save()
 
-    def end(self):
-        self.delete()
+	def end(self):
+		self.delete()
 
-    def next_section(self):
-        self.section_number += 1
-        self.save()
+	def next_section(self):
+		self.section_number += 1
+		self.save()
 
 
 class Participant(models.Model):
-    id = models.CharField(unique=True, primary_key=True, max_length=36, default='')
-    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+	id = models.CharField(unique=True, primary_key=True, max_length=36, default='')
+	lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return str(self.id)
+	def __str__(self):
+		return str(self.id)
 
-    def save(self, *args, **kwargs):
-        self.id = str(uuid4())
-        print(self.id)
-        super(Participant, self).save(*args, **kwargs)
+	def save(self, *args, **kwargs):
+		if self.id is '':
+			self.id = str(uuid4())
+		super(Participant, self).save(*args, **kwargs)
--- boredometer_app/tests.py
+++ boredometer_app/tests.py
@@ -1,104 +1,119 @@
 from django.test import TestCase
-from boredometer_app.models import Lesson
+from boredometer_app.models import Lesson, Participant
+from uuid import uuid4
 from django.core.exceptions import ObjectDoesNotExist
 
 
 class LessonTestCase(TestCase):
+	def setUp(self):
+		self.testCase = Lesson.objects.create(id='ran111')
 
-    def setUp(self):
-        self.testCase = Lesson.objects.create(id='ran111')
+	def tearDown(self):
+		self.testCase.delete()
 
-    def tearDown(self):
-        self.testCase.delete()
+	def test_get_lesson_success(self):
+		"""An existing lesson is successfully retrieved"""
+		self.assertEqual(Lesson.get('ran111').id, 'ran111')
 
-    def test_get_lesson_success(self):
-        """An existing lesson is successfully retrieved"""
-        self.assertEqual(Lesson.get('ran111').id, 'ran111')
+	def test_get_lesson_uppercase_success(self):
+		"""An existing lesson is successfully converted from upper to lower case and retrieved"""
+		self.assertEqual(Lesson.get('RAN111').id, 'ran111')
 
-    def test_get_lesson_uppercase_success(self):
-        """An existing lesson is successfully converted from upper to lower case and retrieved"""
-        self.assertEqual(Lesson.get('RAN111').id, 'ran111')
-
-    def test_get_lesson_failure(self):
-        """Getting a non-existent lesson should throw an ObjectDoesNotExist error"""
-        self.assertRaises(ObjectDoesNotExist, Lesson.get, 'non123')
+	def test_get_lesson_failure(self):
+		"""Getting a non-existent lesson should throw an ObjectDoesNotExist error"""
+		self.assertRaises(ObjectDoesNotExist, Lesson.get, 'non123')
 
 
 class SectionNumberTestCase(TestCase):
+	def setUp(self):
+		self.testCase = Lesson.objects.create(id='ran111')
 
-    def setUp(self):
-        self.testCase = Lesson.objects.create(id='ran111')
+	def tearDown(self):
+		self.testCase.delete()
 
-    def tearDown(self):
-        self.testCase.delete()
+	def test_next_section_from_one(self):
+		"""Clicking next section should increase section number from one to two"""
+		self.testCase.section_number = 0
+		self.testCase.next_section()
+		self.assertEqual(self.testCase.section_number, 1)
 
-    def test_next_section_from_one(self):
-        """Clicking next section should increase section number from one to two"""
-        self.testCase.section_number = 0
-        self.testCase.next_section()
-        self.assertEqual(self.testCase.section_number, 1)
-
-    def test_next_section_from_eleven(self):
-        """Clicking next section should increase section number from eleven to twelve"""
-        self.testCase.section_number = 11
-        self.testCase.next_section()
-        self.assertEqual(self.testCase.section_number, 12)
+	def test_next_section_from_eleven(self):
+		"""Clicking next section should increase section number from eleven to twelve"""
+		self.testCase.section_number = 11
+		self.testCase.next_section()
+		self.assertEqual(self.testCase.section_number, 12)
 
 
 class BoredTestCase(TestCase):
+	def setUp(self):
+		self.testCase = Lesson.objects.create(id='ran111')
 
-    def setUp(self):
-        self.testCase = Lesson.objects.create(id='ran111')
+	def tearDown(self):
+		self.testCase.delete()
 
-    def tearDown(self):
-        self.testCase.delete()
+	def test_add_bored_from_zero(self):
+		"""Adding a participant should increase participants from 0 to 1"""
+		self.testCase.amount_bored = 0
+		self.testCase.add_to_bored()
+		self.assertEqual(self.testCase.amount_bored, 1)
 
-    def test_add_bored_from_zero(self):
-        """Adding a participant should increase participants from 0 to 1"""
-        self.testCase.amount_bored = 0
-        self.testCase.add_to_bored()
-        self.assertEqual(self.testCase.amount_bored, 1)
+	def test_add_bored_from_six(self):
+		"""Adding a participant should increase participants from 9 to 10"""
+		self.testCase.amount_bored = 6
+		self.testCase.add_to_bored()
+		self.assertEqual(self.testCase.amount_bored, 7)
 
-    def test_add_bored_from_six(self):
-        """Adding a participant should increase participants from 9 to 10"""
-        self.testCase.amount_bored = 6
-        self.testCase.add_to_bored()
-        self.assertEqual(self.testCase.amount_bored, 7)
-
-    def test_clear_bored(self):
-        """Clearing the bored participants should result in 0 bored"""
-        self.testCase.amount_bored = 18
-        self.testCase.clear_bored()
-        self.assertEqual(self.testCase.amount_bored, 0)
+	def test_clear_bored(self):
+		"""Clearing the bored participants should result in 0 bored"""
+		self.testCase.amount_bored = 18
+		self.testCase.clear_bored()
+		self.assertEqual(self.testCase.amount_bored, 0)
 
 
 class ParticipantsTestCase(TestCase):
+	def setUp(self):
+		self.testCase = Lesson.objects.create(id='ran111')
 
-    def setUp(self):
-        self.testCase = Lesson.objects.create(id='ran111')
+	def tearDown(self):
+		self.testCase.delete()
 
-    def tearDown(self):
-        self.testCase.delete()
+	def test_add_new_participant(self):
+		"""Adding a participant with no current ID should return a new UUID and increase participants by one"""
+		self.testCase.participants = 0
+		participant_id = self.testCase.add_participant()
+		self.assertEqual(len(participant_id), 36)
+		self.assertEqual(self.testCase.participants, 1)
 
-    def test_add_participants_from_zero(self):
-        """Adding a participant should increase participants from 0 to 1"""
-        self.testCase.participants = 0
-        self.testCase.add_participant()
-        self.assertEqual(self.testCase.participants, 1)
+	def test_add_existing_participant(self):
+		"""Adding a participant who is already in the lesson should return the same UUID and not increase participants"""
+		self.participant = Participant.objects.create(lesson=self.testCase)
+		self.testCase.participants = 0
+		participant_id = self.testCase.add_participant(self.participant.id)
+		self.assertEqual(participant_id, self.participant.id)
+		self.assertEqual(self.testCase.participants, 0)
 
-    def test_add_participants_from_nine(self):
-        """Adding a participant should increase participants from 9 to 10"""
-        self.testCase.participants = 9
-        self.testCase.add_participant()
-        self.assertEqual(self.testCase.participants, 10)
+	def test_add_new_participant_with_id(self):
+		"""Adding a participant with an existing ID who is not in the current lesson should return ID and increase
+		participants by one"""
+		self.testCase.participants = 0
+		random_participant_id = str(uuid4())
+		participant_id = self.testCase.add_participant(random_participant_id)
+		self.assertEqual(participant_id, random_participant_id)
+		self.assertEqual(self.testCase.participants, 1)
+
+
+def test_add_participants_from_nine(self):
+	"""Adding a participant should increase participants from 9 to 10"""
+	self.testCase.participants = 9
+	self.testCase.add_participant()
+	self.assertEqual(self.testCase.participants, 10)
 
 
 class DeleteTestCase(TestCase):
+	def setUp(self):
+		self.testCase = Lesson.objects.create(id='ran111')
 
-    def setUp(self):
-        self.testCase = Lesson.objects.create(id='ran111')
-
-    def test_delete_case(self):
-        """Running the end lesson function should delete the lesson"""
-        self.testCase.end()
-        self.assertRaises(ObjectDoesNotExist, Lesson.get, 'ran111')
+	def test_delete_case(self):
+		"""Running the end lesson function should delete the lesson"""
+		self.testCase.end()
+		self.assertRaises(ObjectDoesNotExist, Lesson.get, 'ran111')
